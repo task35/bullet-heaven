90e75b0f4216cac1ac10322322e2255d24e8f1b5
Ramsey Nasser <ram@nas.sr>
2017-07-20 21:00:34 -0400

    Add visible? to core

--- Assets/task35/bullet_heaven/core.clj
+++ Assets/task35/bullet_heaven/core.clj
@@ -1,7 +1,7 @@
 (ns task35.bullet-heaven.core
   (:use arcadia.core
         arcadia.linear)
-  (:import [UnityEngine GameObject Vector2 Rigidbody2D]))
+  (:import [UnityEngine Camera GameObject Vector2 Rigidbody2D]))
 
 (defn move-2d [^GameObject go ^Vector2 v]
   (let [rb (cmpt go Rigidbody2D)
@@ -9,3 +9,9 @@
     (.. rb
         (MovePosition
           (v2+ position v)))))
+
+(defn visible? [^GameObject go]
+  (let [vp (.WorldToViewportPoint Camera/main 
+                                  (.. go transform position))]
+    (and (< 0 (.x vp) 1)
+         (< 0 (.y vp) 1))))
