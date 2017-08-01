ff29a8e80d756955e61f856555159aebc1bb2870
timsgardner <timsgardner@gmail.com>
2017-08-01 18:43:35 -0400

    fixing thing

--- Assets/task35/bullet_heaven/core.clj
+++ Assets/task35/bullet_heaven/core.clj
@@ -2,7 +2,7 @@
   (:use arcadia.core
         arcadia.linear)
   (:require [magic.api :as m])
-  (:import [Camera UnityEngine GameObject Vector2 Rigidbody2D]))
+  (:import [UnityEngine Camera GameObject Vector2 Rigidbody2D]))
 
 ;; throws exception when we try to make m/defn
 (defn move-2d [^GameObject go ^Vector2 v]
