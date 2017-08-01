252c37f7ef5144169e6962c103fadc96727da9ae
timsgardner <timsgardner@gmail.com>
2017-08-01 18:34:27 -0400

    bumping Arcadia version

--- Assets/task35/bullet_heaven/core.clj
+++ Assets/task35/bullet_heaven/core.clj
@@ -1,11 +1,13 @@
 (ns task35.bullet-heaven.core
   (:use arcadia.core
         arcadia.linear)
+  (:require [magic.api :as m])
   (:import [UnityEngine GameObject Vector2 Rigidbody2D]))
 
+;; throws exception when we try to make m/defn
 (defn move-2d [^GameObject go ^Vector2 v]
   (let [rb (cmpt go Rigidbody2D)
         position (.position rb)]
     (.. rb
-        (MovePosition
-          (v2+ position v)))))
+      (MovePosition
+        (v2+ position v)))))
